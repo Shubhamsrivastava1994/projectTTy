122ef71613ee1013f6c764347ec97f96ce077407
Shubhamsrivastava1994 <shubhamsrivastava4971gmail.com>
2023-07-09 11:36:10 +0530

    insertData

--- updateDatainmongo.js
+++ updateDatainmongo.js
@@ -1,23 +1,26 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const mongouir = process.env.MONGO_URI;
+
 
 // Connection URI
-const uri = 'mongodb+srv://shubhamsrivastava4971:abhisheksrivastava@cluster0.89snqls.mongodb.net/';
 
 // Database Name
 const dbName = 'Shubham_dbs';
 
 async function insertData() {
     try {
-        const client = await MongoClient.connect(uri);
+        const client = await MongoClient.connect(mongouir);
         const dataBase = client.db(dbName).collection('name');
-        const data = await dataBase.findOne({ empId: 4 })
-        const { empName } = data;
-        console.log(data);
-        await dataBase.updateOne({ empId: 4 }, {
-            "$set": {
-                "family": ["lalita", "kavita", "ravieena", "aveena"]
+        //  const data = await dataBase.findOne({ empId: 4 })
+        //  const { empName } = data;
+        // console.log(data);
+        await dataBase.insertOne(
+            {
+                "family": ["lalita", "kavita", "ravieena", "aveena"],
+                "name": "Shubham Srivastava"
             }
-        })
+        )
         console.log("DATA UPDATED")
         client.close();
     } catch (error) {
